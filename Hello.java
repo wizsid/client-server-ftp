74f928194a3b4395999efe360e673444edf310f3
Rolandas Burbulis <rolandasburbulis@gmail.com>
2016-11-20 20:37:27 -0500

    deleting hello.java

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello world");
-		System.out.println("Hello world 2");
-		System.out.println("Hello world 3");
-	}
-}
